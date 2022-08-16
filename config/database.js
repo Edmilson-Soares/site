d6338c6b4d051ed5ae0e8983eb511cb5633e1924
jvperoni <almeidaj@alunos.utfpr.edu.br>
2022-08-16 19:06:31 +0100

    v1.1

--- config/database.js
+++ config/database.js
@@ -1,11 +1,31 @@
 const path = require('path');
 
+
+/*
+
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        client: 'sqlite',
+        connection: {
+            filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        },
+        useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
+});
+
+
+*/
+
+module.exports = ({ env }) => ({
+    connection: {
+        client: 'postgres',
+        connection: {
+            host: env('DATABASE_HOST', ''),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', ''),
+            user: env('DATABASE_USERNAME', ''),
+            password: env('DATABASE_PASSWORD', ''),
+            ssl: env.bool('DATABASE_SSL', false),
+        },
+    },
 });
